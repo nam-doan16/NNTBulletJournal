520974f727335ef2c80c6ac957aab90bb51d0dca
Tony Giech <76697442+tGiech22@users.noreply.github.com>
2023-06-20 22:10:05 -0400

    Tony branch (#37)
     abstracted changing theme methods

--- src/main/java/cs3500/pa05/view/CalendarViewImp.java
+++ src/main/java/cs3500/pa05/view/CalendarViewImp.java
@@ -1,9 +1,13 @@
 package cs3500.pa05.view;
 
 import cs3500.pa05.controller.CalendarController;
+import cs3500.pa05.model.theme.AbstTheme;
+import cs3500.pa05.model.theme.Minimalistic;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class CalendarViewImp implements CalendarView {
   private FXMLLoader loader;
@@ -12,6 +16,11 @@ public class CalendarViewImp implements CalendarView {
     this.loader = new FXMLLoader();
     this.loader.setLocation(getClass().getClassLoader().getResource("Calendar.fxml"));
     this.loader.setController(c);
+//    Parent root = this.loader.getRoot();
+//    Scene scene = new Scene(root);
+//    String css = this.getClass().getResource("Minimal.css").toExternalForm();
+//    scene.getStylesheets().add(css);
+//    stage.setScene(scene);
   }
 
   @Override
@@ -22,5 +31,17 @@ public class CalendarViewImp implements CalendarView {
     } catch (IOException exc) {
       throw new IllegalStateException("Unable to load layout.");
     }
+  }
+
+  public void setTheme(AbstTheme theme) {
+
+  }
+
+  public void changeScene(Stage stage) {
+    Parent root = this.loader.getRoot();
+    Scene scene = new Scene(root);
+    String css = this.getClass().getResource("Minimal.css").toExternalForm();
+    scene.getStylesheets().add(css);
+    stage.setScene(scene);
   }
 }
