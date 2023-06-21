dad543a559c4692f1562cf0dbe3aba0f319ec525
tGiech22 <tonygiech@gmail.com>
2023-06-21 11:22:47 -0400

    added javadocs for TaskEventView

--- src/main/java/cs3500/pa05/view/TaskEventView.java
+++ src/main/java/cs3500/pa05/view/TaskEventView.java
@@ -2,11 +2,16 @@ package cs3500.pa05.view;
 
 import javafx.scene.Scene;
 
+/**
+ * the view for the TaskEvent part of the GUI
+ */
 public interface TaskEventView {
   /**
-   * Loads a scene from a welcome GUI layout.
+   * Loads a scene for a TaskEvent part of the GUI layout.
    *
    * @return the layout
+   *
+   * @throws IllegalStateException if unable to load layout
    */
   Scene load() throws IllegalStateException;
 }
