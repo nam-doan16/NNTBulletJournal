8c7ff1008cdc07c1ec2d8f67c85edd946dc982f8
tGiech22 <tonygiech@gmail.com>
2023-06-21 11:37:10 -0400

    added javadocs for WelcomeView

--- src/main/java/cs3500/pa05/view/WelcomeViewImp.java
+++ src/main/java/cs3500/pa05/view/WelcomeViewImp.java
@@ -6,15 +6,31 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
+/**
+ * implementation for the WelcomeView
+ */
 public class WelcomeViewImp implements WelcomeView{
 
   private FXMLLoader loader;
+
+  /**
+   * constructor
+   *
+   * @param controller the WelcomeController
+   */
   public WelcomeViewImp(WelcomeController controller) {
     this.loader = new FXMLLoader();
     this.loader.setLocation(getClass().getClassLoader().getResource("Welcome.fxml"));
     this.loader.setController(controller);
   }
 
+  /**
+   * Loads a scene from a welcome GUI layout.
+   *
+   * @return the layout
+   *
+   * @throws IllegalStateException if unable to load layout
+   */
   @Override
   public Scene load() {
     try {
